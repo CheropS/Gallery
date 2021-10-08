83d7ced6a365130fea60b8b9408560439895c3f5
Sharon Cherop <sh.cherop@gmail.com>
2021-10-08 13:13:27 +0300

    changed project name to gallery

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,4 @@
-"""galapp URL Configuration
+"""gallery URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
