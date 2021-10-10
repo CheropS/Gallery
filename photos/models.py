98ce89aff4983bd40b16368e184dd76899821315
Sharon Cherop <sh.cherop@gmail.com>
2021-10-10 13:47:23 +0300

    new model created

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_id=models.CharField(max_length=1000)
+    image_name=models.CharField(max_length=30)
+    image_description=models.CharField(max_length=50)
+
