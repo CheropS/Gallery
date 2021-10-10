59177969be167b5fb68305d7e232b266ca65a117
Sharon Cherop <sh.cherop@gmail.com>
2021-10-10 22:36:53 +0300

    running tests on models.py

--- photos/models.py
+++ photos/models.py
@@ -3,27 +3,28 @@ from django.db import models
 
 # Create your models here.
 
-
-class Location(models.Model):
-    image_location=models.CharField(max_length=30)
-
-    def __str__(self):
-        return self.image_location
-
 class Category(models.Model):
-    image_category=models.CharField(max_length=20)
+    category=models.CharField(max_length=20)
     
 
     def __str__(self):
-        return self.image_category
+        return self.category
 
+class Location(models.Model):
+    location=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+        
 class Image(models.Model):
-    image_id=models.CharField(max_length=1000)
-    image_name=models.CharField(max_length=30)
-    image_description=models.CharField(max_length=50)
-    image_location=models.ForeignKey(Location, on_delete=models.CASCADE)
-    image_category=models.ManyToManyField(Category)
+    image_id=models.ImageField(null=False, blank=False)
+    name=models.CharField(max_length=30)
+    description=models.CharField(max_length=500)
+    location=models.ForeignKey(Location, on_delete=models.CASCADE)
+    category=models.ManyToManyField(Category)
 
     def __str__(self):
         return self.name
+
+
 
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
 # Create your tests here.
+class ImageTestCase(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.sharry=Image(image_id='1', image_name='cat', image_description='cat purring', image_location='nairobi', image_category='pets')
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sharry, Image))
