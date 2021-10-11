75a7a73b6d59dbd76d5403e969e4bf76ef896d0d
Sharon Cherop <sh.cherop@gmail.com>
2021-10-11 11:26:36 +0300

    creating a loop for images

--- photos/models.py
+++ photos/models.py
@@ -15,11 +15,27 @@ class Location(models.Model):
 
     def __str__(self):
         return self.location
+    @classmethod
+    def get_locations(cls):
+        return Location.objects.all()
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name=value)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+
     
           
 
 class Image(models.Model):
-    image_id=models.ImageField(null=False, blank=False)
+    image_id=models.CharField(max_length=100)
+    image=models.ImageField(null=False, blank=False)
     name=models.CharField(max_length=30)
     description=models.CharField(max_length=500)
     location=models.ForeignKey(Location, on_delete=models.CASCADE)
@@ -30,6 +46,19 @@ class Image(models.Model):
     
     def save_image(self):
         self.save()
+    def update_image(self,value):
+        self.image=value
+        self.save_image()
 
-
-
+    @classmethod
+    def search_by_id(cls, search_term):
+        photos=cls.objects.filter(id__icontains=search_term)
+        return photos
+   
+    @classmethod
+    def filter_by_location(cls, location):
+        return Image.objects.filter(location__name=location).all()
+        
+    @classmethod
+    def search_by_category(cls, category):
+        return cls.objects.filter(category__name__icontains=category)
